0884eaf84b5b672154ce321910a80db9155e9978
prabu <prabuwijaya267@gmail.com>
2025-09-27 09:37:31 +0800

    first commit

---
+++ halo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    int num=2;
+    printf("%d" , num);
+    return 0;
+}
---
+++ halo.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main () {
+    int num = 2;
+    printf("%d", num);
+    return 0;
+}
